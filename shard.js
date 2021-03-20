fb7a17cfe07300bca9021ec54c170d1e2b3beccd
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-03-20 18:22:39 +0200

    Delete shard.js
    Wasent being used and unnecessary

--- shard.js
+++
@@ -1,6 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const configs = require('./configs.json')
-const manager = new ShardingManager('./bot.js', { token: configs.token });
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
