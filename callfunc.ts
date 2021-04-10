069d96d84c099b256eefc992b71eb7384484df34
SirObsidian <70753342+SirObby@users.noreply.github.com>
2021-04-10 15:39:46 +0300

    Delete callfunc.ts

--- callfunc.ts
+++
@@ -1,9 +0,0 @@
-const bot = require('./bot.js')
-
-exports.setDisp = async (message) => {
-    bot.setDispatch(message, undefined)
-}
-
-exports.setSlashDisp = async (interaction, member) => {
-    bot.setDispatchSlash(interaction, member)
-}
